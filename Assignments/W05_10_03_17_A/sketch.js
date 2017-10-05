59ef802be679e34c1d370b5d0d8c5b7c9a8b2780
Liam Panero <liampanero@gmail.com>
2017-10-05 08:49:02 -0400

    Add files via upload

--- Assignments/W05_10_03_17_A/sketch.js
+++ Assignments/W05_10_03_17_A/sketch.js
@@ -1,69 +1,64 @@
 
-var series2 = [];
-var j = 0;
 var strtCount;
 var count;
-var rotRate = [];
+var rot = [];
+var transX = [];
+
 function setup(){
     frameRate(30);
     createCanvas(600,600);
     background(220);
     strtCount = frameCount;
     for(var i = 0; i<2;i++){
-        rotRate[i] = [];
+        rot[i] = [];
         for(var o = 0; o<3;o++){
-            rotRate[i][o] = .4; 
+            rot[i][o] = .4; 
         }
     }
-    
+    for(var i = 0; i<3;i++){
+        transX[i] = 200*i+100;
+    }
 }
     
 function draw(){
-    
     count = frameCount - strtCount;
-    push();
-    while(j == 0){
-            for(var i=0;i<3;i++){
-                push();
-                if(i == 0){
-                    translate(100,100);
-                }else{
-                    translate(i*200,100);
-                }  
-                rotate(rotRate[1][i]);
-                rect(0,0,50,50);
-                pop();
-            } 
-            j++;
+ 
+    push(); 
+        translate(100,100);
+        rotate(rot[1][0]*0);
+        rect(0,0,50,50);
+    pop();
+    
+    push(); 
+        translate(300,100);
+        rotate(rot[1][2]*1);
+        rect(0,0,50,50);
+    pop();
+    
+    push(); 
+        translate(500,100);
+        rotate(rot[1][3]*2);
+        rect(0,0,50,50);
+    pop();
+    
+    for(var i=0;i<3;i++){
+        push();
+        translate(transX[i],400);
+        
+        if(rot[0][i] >= PI*2){
+            rot[0][i] = rot[1][i];  
+        }else if(count==1){
+            background(220);
+            rot[0][i] += (i/5)+.1;
         }
-        j=0;
-    push();
-    while(j == 0){
-        for(var i=0;i<3;i++){
-            push();
-            if(i == 0){
-                translate(100,400);
-            }else{
-                translate(i*200,400);
-            }
-
-            if(rotRate[0][i] >= PI*2){
-                rotRate[0][i] = rotRate[1][i];
-            }else if(count==1){
-                background(220);
-                rotRate[0][i] += (i/5)+.1;
-            }
-            console.log(rotRate);   
-            rotate(rotRate[0][i]);
-            rect(0,0,50,50);
-            pop();
-        } 
-        j++;
-        if(count == 45){
-            strtCount = frameCount;    
-        }
+          
+        rotate(rot[0][i]);
+        rect(0,0,50,50);
+        pop();
+    } 
+    if(count == 45){
+        strtCount = frameCount;    
     }
-    j=0;
     count++;
 }
     
